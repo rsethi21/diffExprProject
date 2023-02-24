2fa9cd76e2e0a142415d096dd7a988f52baac7ef
Rohan Sethi <rsethi1@luc.edu>
2023-02-24 17:20:11 -0600

    Added a shell script to download data and folder with data

---
+++ data.sh
@@ -0,0 +1,10 @@
+mkdir data
+cd data
+echo 'Downloading SRX2896360'
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896360
+echo 'Downloading SRX2896363'
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896363
+echo 'Downloading SRX2896374'
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896374
+echo 'Downloading SRX2896375'
+wget https://www.ncbi.nlm.nih.gov/sra/SRX2896375
