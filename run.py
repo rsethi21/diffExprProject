bb6a3f26d043a299254696e6c0b9af706beeb8fd
Rohan Sethi <rsethi1@luc.edu>
2023-03-14 23:14:54 -0500

    added logging functionality for quanitification of TPMs

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description='differential expression pipeline')
 parser.add_argument('-s', '--input', help='input file with NCBI links', required=False, default='./data/links/testInput.txt')
 parser.add_argument('-i', '--index', help='input accession id for index', required=False, default='NC_006273.2')
 parser.add_argument('-e', '--email', help='input email for NCBI access', required=True)
-parser.add_argument('-m', '--metatable', help='metatable tab deliminated', required=False, default='./data/metatable')
+parser.add_argument('-m', '--metatable', help='metatable tab deliminated', required=False, default='./data/metatable.tsv')
 
 args = parser.parse_args()
 
@@ -14,7 +14,8 @@ args = parser.parse_args()
 # os.system('python3 ./scripts/decompressFastQ.py -i ./data/raw -o ./data/fastq')
 # os.system(f'python3 ./scripts/retreiveIndex.py -e {args.email} -a {args.index} -o ./data/index/raw.fasta -l ./PipelineProject.log')
 # os.system(f'time kallisto index -i ./data/index/index.idx ./data/index/raw.fasta')
-for fn in os.listdir('./data/raw'):
-    f1 = f'./data/fastq/{fn}_1.fastq'
-    f2 = f'./data/fastq/{fn}_2.fastq'
-    os.system(f'time kallisto quant -i ./data/index/index.idx -o ./results/{fn} -b 30 -t 4 {f1} {f2}')
+# for fn in os.listdir('./data/raw'):
+    # f1 = f'./data/fastq/{fn}_1.fastq'
+    # f2 = f'./data/fastq/{fn}_2.fastq'
+    # os.system(f'time kallisto quant -i ./data/index/index.idx -o ./results/{fn} -b 30 -t 4 {f1} {f2}')
+os.system(f'python3 ./scripts/quantify.py -m {args.metatable} -r ./results -l ./PipelineProject.log')
---
+++ scripts/quantify.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='TPM calculations for each sample')
+parser.add_argument('-m', '--metatable', help='metatable of samples', required=True)
+parser.add_argument('-r', '--resultsFolder', help='path to results folder', required = True)
+parser.add_argument('-l', '--logfile', help='input logfile path', required=True)
+
+def output(metaFile, resultsFolder):
+    meta = pd.read_table(metaFile)
+    outputDict = {}
+    for sampleID in os.listdir(resultsFolder):
+        sample = int(meta[meta['name'] == sampleID][ 'sample'])
+        condition = int(meta[meta['name'] == sampleID]['condition'])
+        sampleAbundance = pd.read_table(f'{resultsFolder}/{sampleID}/abundance.tsv')
+        tpms = sampleAbundance['tpm']
+        outputDict[sampleID] = {'sample': sample, 'condition': condition, 'min_tpm': round(tpms.min(), 1), 'med_tpm': round(tpms.median(), 1), 'mean_tpm': round(tpms.mean(), 1), 'max_tpm': round(tpms.max(), 1)}
+    return pd.DataFrame.from_dict(outputDict, orient='index')
+if __name__ == '__main__':
+    args = parser.parse_args()
+    df = output(args.metatable, args.resultsFolder)
+    with open(args.logfile, 'a') as file:
+        file.write('\n')
+    df.to_csv(args.logfile, sep='\t', header=True, index=False, mode='a', encoding='ascii')
