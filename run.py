edc7fcd9ad7bde4abc362c197a0e95e8e1380ccd
Rohan Sethi <rsethi1@luc.edu>
2023-03-14 21:55:33 -0500

    restructured code

--- run.py
+++ run.py
@@ -5,13 +5,14 @@ parser = argparse.ArgumentParser(description='differential expression pipeline')
 parser.add_argument('-s', '--input', help='input file with NCBI links', required=False, default='./data/links/testInput.txt')
 parser.add_argument('-i', '--index', help='input accession id for index', required=False, default='NC_006273.2')
 parser.add_argument('-e', '--email', help='input email for NCBI access', required=True)
+parser.add_argument('-m', '--metatable', help='metatable tab deliminated', required=False, default='./data/metatable')
 
 args = parser.parse_args()
 
-# os.system('chmod +x ./data/data.sh')
-# os.system(f'./data/data.sh {args.input}')
-# os.system('python3 ./data/decompressFastQ.py -i ./data/raw -o ./data/fastq')
-# os.system(f'python3 ./data/retreiveIndex.py -e {args.email} -a {args.index} -o ./data/index/raw.fasta -l ./PipelineProject.log')
+# os.system('chmod +x ./scripts/data.sh')
+# os.system(f'./scripts/data.sh {args.input}')
+# os.system('python3 ./scripts/decompressFastQ.py -i ./data/raw -o ./data/fastq')
+# os.system(f'python3 ./scripts/retreiveIndex.py -e {args.email} -a {args.index} -o ./data/index/raw.fasta -l ./PipelineProject.log')
 # os.system(f'time kallisto index -i ./data/index/index.idx ./data/index/raw.fasta')
 for fn in os.listdir('./data/raw'):
     f1 = f'./data/fastq/{fn}_1.fastq'
