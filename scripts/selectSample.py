b816a609f2b311aacca419abf1efd1990865a112
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-03-17 00:28:01 -0500

    Update selectSample.py

--- scripts/selectSample.py
+++ scripts/selectSample.py
@@ -1,8 +1,10 @@
+# imports
 from Bio import SeqIO
 import argparse
 import pandas as pd
 from tqdm import tqdm
 
+# arguments and flags whcih are explained further in the help and description
 parser = argparse.ArgumentParser(description='select a subset of fastq records')
 parser.add_argument('-i', '--input', help='input folder for reads', required=True)
 parser.add_argument('-a', '--amount', help='amount of fastq records to keep', required=True)
@@ -10,30 +12,37 @@ parser.add_argument('-o', '--output', help='output folder', required=False, defa
 parser.add_argument('-m', '--metafile', help='input metafile path', required=True)
 
 def extractSequences(fq1, fq2):
-    with open(fq1, 'r') as f1:
+    '''
+    given a set a paired end reads, extracting fastq records in a list
+    '''
+    with open(fq1, 'r') as f1: # openning the forward read
         fastq1 = list(SeqIO.parse(f1, 'fastq'))
 
-    with open(fq2, 'r') as f2:
+    with open(fq2, 'r') as f2: # opening the reverse read
         fastq2 = list(SeqIO.parse(f2, 'fastq'))
     
     return fastq1, fastq2
 
 def takeSample(seqs1, seqs2, amount, out1, out2):
+    '''
+    given a set of paired end reads, extracting subset of fastq records and storing in separate test data folder
+    '''
     with open(out1, 'w') as o1:
-        SeqIO.write(seqs1[:int(amount)], o1, 'fastq')
+        SeqIO.write(seqs1[:int(amount)], o1, 'fastq') # saves the sequences from forward reads by splicing the list to grab only the amount specified
 
     with open(out2, 'w') as o2:
-        SeqIO.write(seqs2[:int(amount)], o2, 'fastq')
+        SeqIO.write(seqs2[:int(amount)], o2, 'fastq') # saves the sequences from reverse reads by splicing the list to grab only the amount specified
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    m = pd.read_table(args.metafile)
-    names = m['sample']
-    for n in tqdm(names):
-        forward = f'{args.input}/{n}_1.fastq'
-        reverse = f'{args.input}/{n}_2.fastq'
-        fq1, fq2 = extractSequences(forward, reverse)
-        forwardOut = f'{args.output}/{n}_1.fastq'
-        reverseOut = f'{args.output}/{n}_2.fastq'
-        takeSample(fq1, fq2, args.amount, forwardOut, reverseOut)
+    # runs all in the command line
+    args = parser.parse_args() # creating command line arguments instance
+    m = pd.read_table(args.metafile) # reading in the metadata table
+    names = m['sample'] # grabbing the sample names from this table
+    for n in tqdm(names): # for each sample, open the assosicated fastq files already fastq-dumped (tqdm creates a progress bar to see how far in the loop from the command line)
+        forward = f'{args.input}/{n}_1.fastq' # forward read path _1
+        reverse = f'{args.input}/{n}_2.fastq' # reverse read path _2
+        fq1, fq2 = extractSequences(forward, reverse) # calling extract function
+        forwardOut = f'{args.output}/{n}_1.fastq' # output path forward reads subset
+        reverseOut = f'{args.output}/{n}_2.fastq' # output path reverse reads subset
+        takeSample(fq1, fq2, args.amount, forwardOut, reverseOut) # calling on extractng sample function above
