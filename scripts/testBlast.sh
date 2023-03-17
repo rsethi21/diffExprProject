4bba2bde8ed99d8e24c452d5b79a8101b8d9733d
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-03-17 00:35:13 -0500

    Update testBlast.sh

--- scripts/testBlast.sh
+++ scripts/testBlast.sh
@@ -1,9 +1,7 @@
+# input arguments from command line; query protein sequence, database to query against, and results output path
+# this is for the test run so almost exact copy to the entire run one with some small change
 QUERY=$1
 DB=$2
 OUTresults=$3
 
-echo $QUERY
-echo $DB
-echo $OUTresults
-
-tblastn -query $QUERY -db $DB -max_hsps 1 -out $OUTresults -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitle"
+tblastn -query $QUERY -db $DB -max_hsps 1 -out $OUTresults -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitle" # running the blast algorithm from the command line to find strains that may express this significant differentially expressed gene from samples
