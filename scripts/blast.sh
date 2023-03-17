23d6f52637a6e1532efc15a280a8ae9ca5eeca3a
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-03-16 23:29:44 -0500

    Update blast.sh

--- scripts/blast.sh
+++ scripts/blast.sh
@@ -1,9 +1,9 @@
-NAME=$1
-REF=$2
-QUERY=$3
-OUTdb=$4
-OUTresults=$5
+NAME=$1 # name of output database
+REF=$2 # path to fasta file of genomic data for species
+QUERY=$3 # fasta of query sequence (most differentially expressed)
+OUTdb=$4 # output datat folder for database
+OUTresults=$5 # output path of the blast search
 
-makeblastdb -in $REF -out $OUTdb -title $NAME -dbtype nucl
+makeblastdb -in $REF -out $OUTdb -title $NAME -dbtype nucl # this si
 
 tblastn -query $QUERY -db $OUTdb -max_hsps 1 -out $OUTresults -outfmt "6 sacc pident length qstart qend sstart send bitscore evalue stitle"
